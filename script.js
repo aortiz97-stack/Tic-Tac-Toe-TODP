efea6a84e68d354c171a5d475de5a44286fa57d4
Armando <armando.ortiz.jr.19@outlook.com>
2022-12-28 19:27:29 -0600

    Add feature for switching between players after turn has ended

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-underscore-dangle */
 const boardGame = (() => {
-  let turnEnded = false;
-  const _gameCompleted = (document) => {
+  const board = [];
+
+  const gameCompleted = (document) => {
     let gameFinished = true;
     const htmlBoard = document.querySelector('.game-board');
     const cells = htmlBoard.children;
@@ -15,59 +16,88 @@ const boardGame = (() => {
     return gameFinished;
   };
 
-  const _addEventListener = (cell, player, document) => {
-    turnEnded = false;
-
-    cell.addEventListener('click', () => {
-      if (cell.innerHTML !== 'X' && cell.innerHTML !== 'O') {
-        cell.innerHTML = player.markerType;
-        turnEnded = true;
+  const _addEventListener = (controlPlayer, otherPlayer, document) => {
+    let clicked = false;
+    const htmlBoard = document.querySelector('.game-board');
+    htmlBoard.addEventListener('click', (e) => {
+      console.log(`target: ${e.target}`);
+      if (e.target.innerHTML !== 'X' && e.target.innerHTML !== 'O') {
+        clicked = true;
+        e.target.innerHTML = controlPlayer.markerType;
+        const oldControlPlayer = controlPlayer;
+        controlPlayer = otherPlayer;
+        otherPlayer = oldControlPlayer;
+        console.log(`controlPlayer: ${controlPlayer.name}`);
+        console.log(`otherPlayer: ${otherPlayer.name}`);
       }
-      if (_gameCompleted(document)) {
+      if (gameCompleted(document)) {
         alert('GAME COMPLETED');
       }
     });
+    return { controlPlayer, otherPlayer, clicked };
   };
-  const _displayBoard = (document, player) => {
-    const board = document.querySelector('.game-board');
+
+  const playOneRound = (document, controlPlayer, otherPlayer) => {
+    const HTMLboard = document.querySelector('.game-board');
+    HTMLboard.innerHTML = '';
     for (let i = 1; i < 10; i += 1) {
       const cell = document.createElement('div');
       cell.classList.add(`cell${i}`);
-      _addEventListener(cell, player, document);
-      board.appendChild(cell);
+      HTMLboard.appendChild(cell);
     }
-  };
-  const createBoard = (document, player) => {
-    const board = [];
+    const newPlayerPositions = _addEventListener(controlPlayer, otherPlayer, document);
+    controlPlayer = newPlayerPositions.controlPlayer;
+    otherPlayer = newPlayerPositions.otherPlayer;
 
-    for (let i = 0; i < 3; i += 1) {
-      const row = [];
-      board.push(row);
+    return { controlPlayer, otherPlayer };
+  };
+
+  const _displayBoard = (document, controlPlayer, otherPlayer) => {
+    let newPlayerPositions = playOneRound(document, controlPlayer, otherPlayer);
+    console.log('passed');
+    /* for (let i = 0; i < 1; i += 1) {
+      const newPlayerPositions = playOneRound(document, controlPlayer, otherPlayer);
+      controlPlayer = newPlayerPositions.controlPlayer;
+      otherPlayer = newPlayerPositions.otherPlayer;
+    } */
+    /* let newPlayerPositions = playOneRound(document, controlPlayer, otherPlayer); */
+
+    /*
+    controlPlayer = newPlayerPositions.controlPlayer;
+    otherPlayer = newPlayerPositions.otherPlayer;
+    newPlayerPositions = playOneRound(document, controlPlayer, otherPlayer);*/
+  };
+
+  const createBoard = (document, controlPlayer, otherPlayer) => {
+    if (board.length === 0) {
+      for (let i = 0; i < 3; i += 1) {
+        const row = [];
+        board.push(row);
+      }
     }
-    _displayBoard(document, player);
+    _displayBoard(document, controlPlayer, otherPlayer);
     return { board };
   };
 
-  return { createBoard, turnEnded };
+  return { createBoard, gameCompleted };
 })();
-
 
 const Player = (name, markerType, winningStatus) => {
   const changeWinningStatus = (newWinningStatus) => {
     winningStatus = newWinningStatus;
   };
+  const turnEnded = false;
   return {
-    name, markerType, winningStatus, changeWinningStatus,
+    name, markerType, winningStatus, changeWinningStatus, turnEnded,
   };
 };
 
-
 const playGame = (() => {
   const _initialize = (document) => {
-    const player1Name = prompt('Player one, please enter your name:');
-    const player1Marker = prompt(('Please choose which marker you would like to use (i.e. x or o)')).toUpperCase();
+    const player1Name = 'Armando'; /* prompt('Player one, please enter your name:'); */
+    const player1Marker = 'X'; /* prompt(('Please choose which marker you would like to use (i.e. x or o)')).toUpperCase(); */
     const Player1 = Player(player1Name, player1Marker, 'inconclusive');
-    const player2Name = prompt('Player two, please enter your name:');
+    const player2Name = 'Jose';/* prompt('Player two, please enter your name:'); */
     let player2Marker;
     if (player1Marker === 'X') {
       player2Marker = 'O';
@@ -75,17 +105,20 @@ const playGame = (() => {
       player2Marker = 'X';
     }
     const Player2 = Player(player2Name, player2Marker, 'inconclusive');
-    const board = boardGame.createBoard(document, Player1);
+    const board = boardGame.createBoard(document, Player1, Player2);
     return { board, Player1, Player2 };
   };
 
   const switchTurn = (player1, player2, controller = player1) => {
+    let otherPlayer;
     if (controller === player1) {
       controller = player2;
+      otherPlayer = player1;
     } else {
       controller = player1;
+      otherPlayer = player2;
     }
-    return { controller };
+    return { controller, otherPlayer };
   };
 
   const play = (document) => {
@@ -94,19 +127,21 @@ const playGame = (() => {
     const { Player2 } = gameComponents;
     const { board } = gameComponents;
     let controller = Player1;
+    let otherPlayer = Player2;
 
-    const { turnEnded } = board;
-    if (turnEnded) {
-      controller = switchTurn(Player1, Player2, controller);
-      board.turnEnded = false;
+    if (controller.turnEnded) {
+      const switchingMethod = switchTurn(Player1, Player2, controller);
+      controller = switchingMethod.controller;
+      otherPlayer = switchingMethod.otherPlayer;
+      boardGame.createBoard(document, controller, otherPlayer);
     }
   };
 
   return { play };
 })();
 
-const player = Player('Armando', 'X', 'inconclusive');
-console.log(player.markerType);
-boardGame.createBoard(document, player);
+const player1 = Player('Armando', 'X', 'inconclusive');
+const player2 = Player('Jose', 'O', 'inconclusive');
+boardGame.createBoard(document, player1, player2);
 
 /* playGame.play(document); */
